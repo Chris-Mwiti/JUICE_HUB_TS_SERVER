0fe86c3c954d31c29859e9ea05012a5cc55288fb
Chris-Mwiti <chrismwiti21@gmail.com>
2024-03-24 10:29:27 +0300

    Cors Config updates

--- src/config/corsConfig.ts
+++ src/config/corsConfig.ts
@@ -1,11 +1,10 @@
 import { CorsOptions } from "cors";
 
-const whiteListedDomains:string[] = ['http://localhost:5000', 'http://localhost:3000', 'http://localhost:5173'];
+const whiteListedDomains:string[] = ['http://localhost:5000', 'http://localhost:3000', 'http://localhost:5173', ];
 
 const corsOptions: CorsOptions = {
     origin: (origin, callback) =>{
-        if(whiteListedDomains.includes(origin as string) || !origin) return callback(null, true);
-        callback(new Error("You have no acces for the resources provided by this server"),false);
+        return callback(null, true);
     },
     optionsSuccessStatus: 200,
     credentials: true
